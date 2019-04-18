11a1e4f50ef9787be554a9c3665b47c05e91a428
amark <amarkumar.bhanu@extramarks.com>
2019-04-18 18:14:02 +0530

    python

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
